0614b87379540ed291d43c2334eb39354f73d25c
Dushyant Rawat <dushyant89303@gmail.com>
2026-07-04 09:38:14 +0530

    Add files via upload

---
+++ Day_026/main.py
@@ -0,0 +1,10 @@
+lst = [1,2,3,4,5,6,7,8,9]
+new_lst = [item+1 for item in lst ]
+print(new_lst)
+
+name = "Dushyant"
+letters_list = [char for char in name]
+print(letters_list)
+
+nums = [2*n for n in range(1,5)]
+print(nums)
